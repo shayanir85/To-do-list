3c7c490561ff2f55af9bdba71924f7f0098c3766
shayanir85 <shayaniranpor8@gmail.com>
2025-07-10 11:39:52 +0330

    .

--- js/app.js
+++ js/app.js
@@ -6,20 +6,3 @@ const radio = document.getElementById('radio');
 const edit = document.getElementById('edit');
 const del = document.getElementById('delete');
 let i = 0;
-btn_add.addEventListener('click', () => {
-    if (job.value != '') {
-        ToDoList.innerHTML += `
-        `;
-        i++;
-
-    } else {
-        alert('you must write something.');
-    }
-});
-
-radio.addEventListener('change', () => {
-    strike.innerHTML = ``;
-    strike.innerHTML = `
-    <s>${job.value}</s>
-    `;
-});
