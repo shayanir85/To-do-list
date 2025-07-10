277a66aefc6f83994c311cdb8a7b8f68e09e506e
shayanir85 <shayaniranpor8@gmail.com>
2025-07-10 13:03:50 +0330

    .

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-const ToDoList = document.getElementById('toDo');
-const btn_add = document.getElementById('add');
-const job = document.getElementById('job');
-const strike = document.getElementById('St');
-const radio = document.getElementById('radio');
-const edit = document.getElementById('edit');
-const del = document.getElementById('delete');
-let i = 0;
+tasks = [];
+task = {
+    Date: '',
+    compelete: false,
+    title: '',
+};
