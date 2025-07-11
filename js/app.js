d625c8e5a26d4489a8709ab464ed633b98418cb2
shayanir85 <shayaniranpor8@gmail.com>
2025-07-11 12:25:01 +0330

    updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
+// File: js/app.js
+const date = document.getElementById('date');//gets a date from input
+const add = document.getElementById('add');//add button
+const job = document.getElementById('job');
+const toDo = document.getElementById('toDo'); //Todo section
+const del = document.getElementById('delete');
+const edit = document.getElementById('edit');
+const completed = document.getElementById('complete');
 tasks = [];
 task = {
     Date: '',
-    compelete: false,
+    complete: false,
     title: '',
 };
+
+function addTask() {
+    const newtask = task ;
+    newtask.Date = date.value;
+    newtask.title = job.value;
+    date.value = '';
+    job.value = '';
+    tasks.push(newtask);
+}
